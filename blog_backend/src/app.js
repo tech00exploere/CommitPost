857985a4472f158669804489e1b11c74c42b2bc2
Priyanshu <priyanshu2507.rjs@gmail.com>
2025-12-19 03:22:47 +0530

    Update app.js

--- blog_backend/src/app.js
+++ blog_backend/src/app.js
@@ -7,11 +7,22 @@ import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL,
+  ],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/posts", commentRoutes);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
 
 export default app;
