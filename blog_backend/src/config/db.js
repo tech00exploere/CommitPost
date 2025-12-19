904463be9ae8532e55432c8f22f80ec2bde2ed55
Priyanshu <priyanshu2507.rjs@gmail.com>
2025-12-19 14:06:40 +0530

    Update db.js

--- blog_backend/src/config/db.js
+++ blog_backend/src/config/db.js
@@ -6,12 +6,16 @@ const connectDB = async () => {
       throw new Error("MONGO_URI is missing in .env");
     }
 
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error:");
     console.error(error);
-    process.exit(1);
+    process.exit(1); 
   }
 };
 
