5c6295361214808447d3ba87d6116f843dd45638
Priyanshu <priyanshu2507.rjs@gmail.com>
2025-12-20 08:49:46 +0530

    Update server.js

--- blog_backend/src/server.js
+++ blog_backend/src/server.js
@@ -1,50 +1,12 @@
-import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import connectDB from "./config/db.js";
-
-import authRoutes from "./routes/auth.routes.js";
-import postRoutes from "./routes/post.routes.js";
-import commentRoutes from "./routes/comment.routes.js";
+import app from "./app.js";
 
 dotenv.config();
 
-const app = express();
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // allow requests like Postman or server-side (no origin)
-      if (!origin) return callback(null, true);
-
-      const allowedOrigins = [
-        "http://localhost:5173",
-        process.env.CLIENT_URL,
-      ].filter(Boolean); // removes undefined
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-
-
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-
-app.get("/api/health", (req, res) => {
-  res.status(200).json({ status: "OK" });
-});
-
 const PORT = process.env.PORT || 5000;
 
+// ===== Connect to MongoDB & Start Server =====
 connectDB()
   .then(() => {
     console.log("MongoDB connected");
