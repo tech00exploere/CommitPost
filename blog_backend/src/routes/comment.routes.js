778c76f330c28b7595d03d529fea3cb66f84b1d0
Priyanshu <priyanshu2507.rjs@gmail.com>
2025-12-20 08:47:03 +0530

    Update comment.routes.js

--- blog_backend/src/routes/comment.routes.js
+++ blog_backend/src/routes/comment.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getComments, addComment } from "../controllers/comment.controller.js";
+import { getComments, addComment, deleteComment } from "../controllers/comment.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true }); // important for req.params.postId
 
-router.get("/:postId/comments", getComments);
-router.post("/:postId/comments", protect, addComment);
+router.get("/", getComments);          // GET /api/posts/:postId/comments
+router.post("/", protect, addComment); // POST /api/posts/:postId/comments
+router.delete("/:commentId", protect, deleteComment); // DELETE /api/posts/:postId/comments/:commentId
 
 export default router;
