0711de9430fc10a6c3147c1f14e4315bba657b66
Priyanshu <priyanshu2507.rjs@gmail.com>
2025-12-20 08:36:06 +0530

    Update comment.controller.js

--- blogging_frontend/src/controllers/comment.controller.js
+++ blogging_frontend/src/controllers/comment.controller.js
@@ -1,23 +1,57 @@
-import mongoose from "mongoose";
+import Comment from "../models/Comment.model.js";
 
-const commentSchema = new mongoose.Schema(
-  {
-    text: {
-      type: String,
-      required: true,
-    },
-    post: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Post",
-      required: true,
-    },
-    author: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User", // 🔥 IMPORTANT
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+// GET comments
+export const getComments = async (req, res) => {
+  try {
+    const comments = await Comment.find({ post: req.params.postId })
+      .populate("author", "name email")
+      .sort({ createdAt: -1 });
 
-export default mongoose.model("Comment", commentSchema);
+    res.json(comments);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to load comments" });
+  }
+};
+
+// ADD comment
+export const addComment = async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    if (!text?.trim()) {
+      return res.status(400).json({ message: "Comment text is required" });
+    }
+
+    const comment = await Comment.create({
+      text,
+      post: req.params.postId,
+      author: req.user._id,
+    });
+
+    const populated = await comment.populate("author", "name email");
+
+    res.status(201).json(populated);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to add comment" });
+  }
+};
+
+// DELETE comment
+export const deleteComment = async (req, res) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    if (comment.author.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+
+    await comment.deleteOne();
+    res.json({ message: "Comment deleted", id: req.params.commentId });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to delete comment" });
+  }
+};
