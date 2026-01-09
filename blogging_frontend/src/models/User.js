1e688d243a83715abac55f815cce5839d0bafc2c
Priyanshu <priyanshu2507.rjs@gmail.com>
2026-01-09 13:29:29 +0530

    Fix formatting issue in User.js

--- blogging_frontend/src/models/User.js
+++ blogging_frontend/src/models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema(
 );
 export const currentUser = {
   _id: "u123",
-  name: "Priyanshu",
+  name: "Priyanshu"
   email: "test@example.com",
   token: "jwt-token-here", // from login
 };
