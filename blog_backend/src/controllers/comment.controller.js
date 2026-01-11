f562c3497dd448feb1a829d3c75ac9709802d659
Priyanshu <priyanshu2507.rjs@gmail.com>
2026-01-11 22:16:02 +0530

    Add deleteComment function for comment removal

--- blog_backend/src/controllers/comment.controller.js
+++ blog_backend/src/controllers/comment.controller.js
@@ -44,3 +44,25 @@ export const addComment=async(req,res)=>{
     res.status(500).json({ message: "Failed to add comment"});
   }
 };
+export const deleteComment = async (req, res) => {
+  try {
+    const { commentId } = req.params;
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    // Optional: allow only author to delete
+    if (comment.author.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Not authorized" });
+    }
+
+    await comment.deleteOne();
+
+    res.status(200).json({ message: "Comment deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete comment" });
+  }
+};
