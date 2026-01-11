2f43a069347ac33fcd85c090355736c2397b90f1
Priyanshu <priyanshu2507.rjs@gmail.com>
2026-01-11 21:49:01 +0530

    Update db.js

--- blog_backend/src/config/db.js
+++ blog_backend/src/config/db.js
@@ -2,22 +2,18 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log("🔍 MONGO_URI exists:", !!process.env.MONGO_URI);
-
     if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is missing");
+      throw new Error("MONGO_URI is not defined");
     }
-
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ MongoDB connection error DETAILS ↓↓↓");
-    console.error("Name:", error.name);
-    console.error("Message:", error.message);
-    console.error("Stack:", error.stack);
-    process.exit(1);
+    const conn = await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true,
+    });
+    console.log(`MongoDB Connected:${conn.connection.host});
+  }catch(error){
+    console.error("MongoDB connection failed");
+    console.error(error.message);
+    throw error;
   }
 };
-
 export default connectDB;
