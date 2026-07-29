f52af19a5dec73b2c3e0c4e63b3fe0d6fdf77e23
Priyanshu <priyanshu2507.rjs@gmail.com>
2026-07-29 23:37:26 +0530

    Update auth.middleware.js

--- blog_backend/src/middlewares/auth.middleware.js
+++ blog_backend/src/middlewares/auth.middleware.js
@@ -5,8 +5,9 @@ export const protect = async (req, res, next) => {
   let token;
   if (req.headers.authorization?.startsWith("Bearer ")) {
     try {
+      //
       token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);   
       req.user = await User.findById(decoded.id).select("-password");
       if (!req.user) return res.status(401).json({ message: "User not found" });
       next();
